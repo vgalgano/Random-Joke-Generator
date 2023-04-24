7fd64006c19d9beb637a274209914fd84c0d546b
Vincenzo <100663687+vgalgano@users.noreply.github.com>
2023-04-24 21:26:52 +0100

    Add files via upload

--- joke.js
+++ joke.js
@@ -1,4 +1,5 @@
 const jokeContainer = document.getElementById("joke");
+const JokeCat = document.getElementById("category");
 const btn = document.getElementById("btn");
 const url = "https://v2.jokeapi.dev/joke/Any?type=single";
 
@@ -7,7 +8,9 @@ let getJoke = () => {
     .then(data => data.json())
     .then(item =>{
         jokeContainer.textContent = `${item.joke}`;
-            });
+        JokeCat.textContent = `${item.category}`;
+            })
+         
     
 }
 btn.addEventListener("click", getJoke);
